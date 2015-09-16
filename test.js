f6e81fe2cc795c6fdb3ab7bc921747775f535b15
Ian Obermiller <ian@obermillers.com>
2015-09-15 17:02:31 -0700

    Tabs to spaces, thanks Atom

--- test.js
+++ test.js
@@ -4,15 +4,15 @@ var friendlyPseudosPlugin = require('./index');
 test('converts onHover to :hover', function(t) {
   t.plan(1);
 
-	var result = friendlyPseudosPlugin({
-		style: {
-			onHover: {color: 'red'}
-		}
-	});
+  var result = friendlyPseudosPlugin({
+    style: {
+      onHover: {color: 'red'}
+    }
+  });
 
-	t.deepEqual(result, {
-		style: {
-			':hover': {color: 'red'}
-		}
-	});
+  t.deepEqual(result, {
+    style: {
+      ':hover': {color: 'red'}
+    }
+  });
 });
