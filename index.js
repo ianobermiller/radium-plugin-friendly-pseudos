5b186259cd462788b00ea6a75f347eba21185c8f
Ian Obermiller <ian@obermillers.com>
2015-09-15 17:03:14 -0700

    Tabs to spaces in index.js as well. Thanks, Atom.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 module.exports = function(config) {
   var newStyle = {};
-	Object.keys(config.style).forEach(function(key) {
-		var newKey = key;
-		if (/^on[A-Z]/.test(key)) {
-			newKey = key.replace(/^on([A-Z])/, function(match, p1) {
-				return ':' + p1.toLowerCase();
-			});
-		}
+  Object.keys(config.style).forEach(function(key) {
+    var newKey = key;
+    if (/^on[A-Z]/.test(key)) {
+      newKey = key.replace(/^on([A-Z])/, function(match, p1) {
+        return ':' + p1.toLowerCase();
+      });
+    }
 
-		newStyle[newKey] = config.style[key];
-	});
-	return {style: newStyle};
+    newStyle[newKey] = config.style[key];
+  });
+  return {style: newStyle};
 };
